3d4fd1b2142a3a2c57130d1c3676d354e4f113a2
Abhilash15 <31845567+Abhilash15@users.noreply.github.com>
2021-04-09 19:16:16 -0500

    made changes to controllers office.js

--- controllers/office.js
+++ controllers/office.js
@@ -12,13 +12,25 @@ exports.office_list = async function(req, res) {
         res.error(500,`{"error": ${err}}`);
         }
     };
-// for a specific office.
-exports.office_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: office detail: ' + req.params.id);
-};
+
 // Handle office create on POST.
-exports.office_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: office create POST');
+exports.office_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new office();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.department = req.body.department;
+    document.strength = req.body.strength;
+    document.size = req.body.size;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+   };
 };
 // Handle office delete form on DELETE.
 exports.office_delete = function(req, res) {
@@ -39,4 +51,15 @@ exports.office_view_all_Page = async function(req, res) {
     catch(err){
     res.error(500,`{"error": ${err}}`);
     }
+    };
+
+    exports.office_detail = async function(req, res) {
+        console.log("detail"  + req.params.id)
+        try {
+            result = await office.findById( req.params.id)
+            res.send(result)
+        } catch (error) {
+            res.status(500)
+            res.send(`{"error": document for id ${req.params.id} not found`);
+        }
     };
