1d4888ee10d93941b28d697dbb4faf04679b9858
Abhilash15 <31845567+Abhilash15@users.noreply.github.com>
2021-04-09 19:40:58 -0500

    added a new put request

--- controllers/office.js
+++ controllers/office.js
@@ -37,8 +37,25 @@ exports.office_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: office delete DELETE ' + req.params.id);
 };
 // Handle office update form on PUT.
-exports.office_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: office update PUT' + req.params.id);
+//exports.office_update_put = function(req, res) {
+ //res.send('NOT IMPLEMENTED: office update PUT' + req.params.id);
+//};
+
+exports.office_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await office.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.department) toUpdate.department = req.body.department;
+        if(req.body.size) toUpdate.size = req.body.size;
+        if(req.body.strength) toUpdate.strength = req.body.strength;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 
 // VIEWS
