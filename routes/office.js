118e295b9d70e4baa2f3b493130b3db2a4547c77
Abhilash15 <31845567+Abhilash15@users.noreply.github.com>
2021-04-24 20:26:04 -0500

    endpoint protected

--- routes/office.js
+++ routes/office.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const office_controllers= require('../controllers/office');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 /* GET office */
 router.get('/', office_controllers.office_view_all_Page);
 
@@ -11,7 +21,7 @@ router.get('/detail', office_controllers.office_view_one_Page);
 router.get('/create', office_controllers.office_create_Page);
 
 /* GET create update page */
-router.get('/update', office_controllers.office_update_Page);
+router.get('/update', secured, office_controllers.office_update_Page);
 
 /* GET create delete page */
 router.get('/delete', office_controllers.office_delete_Page);
